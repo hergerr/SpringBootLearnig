5a9a1aa908db0ad15bd9429bd35acc2a1822ec9a
herrgerr <tymoteusz@frankiewicz.com.pl>
2018-02-12 10:17:10 +0100

    initial commit

---
+++ src/main/java/pl/com/frankiewicz/Application.java
@@ -0,0 +1,39 @@
+package pl.com.frankiewicz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+    @RestController
+    public class Application {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+
+        @RequestMapping(method = RequestMethod.GET, value = "/hello")
+        public String helloWorld() {
+            return "Hello World!";
+        }
+
+        //typy zlozone tez przejda np. Long, Integer
+        @RequestMapping(method = RequestMethod.GET, value = "/hello/{name}/{greeting}")
+        public String helloWorldPath(@PathVariable String name, @PathVariable String greeting) {
+            return "Hello " + name + ", " + greeting + "!";
+        }
+
+        //przez paramentr
+        //niewymagany argument, domyslnie anon
+        @GetMapping("/")
+        public String helloWordParam(@RequestParam(name = "name", required = false, defaultValue = "anon") String name){
+            return "Hello " + name + "!";
+        }
+
+        //przez body
+        @PutMapping //to samo co do gory
+        public String helloWordBody(@RequestBody String name){
+            return "Hello " + name;
+        }
+
+}
